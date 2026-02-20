9899df550447a283e65c4669651656c24077f162
Daan Acohen <daan.acohen@connectingapps.net>
2026-02-20 16:21:47 +0100

    Update run.sh: log OpenSSL version for each Node runtime

--- run.sh
+++ run.sh
@@ -5,15 +5,18 @@ set -e
 
 echo "=== Running with Node $NODE_VERSION_RUN_1 ==="
 nvm use $NODE_VERSION_RUN_1
+echo "OpenSSL Version"
 node -p "process.versions.openssl"
 node dist/index.js
 
 echo "=== Running with Node $NODE_VERSION_RUN_2 ==="
 nvm use $NODE_VERSION_RUN_2
+echo "OpenSSL Version"
 node -p "process.versions.openssl"
 node dist/index.js
 
 echo "=== Running with Node $NODE_VERSION_RUN_3 ==="
 nvm use $NODE_VERSION_RUN_3
+echo "OpenSSL Version"
 node -p "process.versions.openssl"
 node dist/index.js
